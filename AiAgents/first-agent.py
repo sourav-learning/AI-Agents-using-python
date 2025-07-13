214153b7860140d6a7e047a2daf0e11bfac976e8
sourav-learning <chatterjee_sourav2003@yahoo.co.in>
2025-07-13 22:01:16 +0530

    token change

--- AiAgents/first-agent.py
+++ AiAgents/first-agent.py
@@ -1,8 +1,9 @@
 import os
 import streamlit as st
-from langchain.agents import initialize_agent, AgentType
 from langchain_openai import ChatOpenAI
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+#instead of using from env using from streamlit
+#OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 llm=ChatOpenAI(model="gpt-4o-mini", temperature=0.9, openai_api_key=OPENAI_API_KEY)
 prompt="You are a helpful assistant. Answer the following question: {input}"
 #input = input("Enter your question: ")  # To prrompt user for providing input when not using streamlit
